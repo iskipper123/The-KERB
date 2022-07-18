5ab16207e3a336861fb31cb0900df514e0da3641
iskipper123 <iskipper123@gmail.com>
2022-07-18 21:54:18 +0300

    aaa
    aaa

--- js/app.js
+++ js/app.js
@@ -5,3 +5,20 @@ function submitForm(e) {
     e.preventDefault();
     console.log(123);
 }
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'my_db'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
