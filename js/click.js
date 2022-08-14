3e24dac1748cc9047d3a32778e4be3a653317706
iskipper123 <iskipper123@gmail.com>
2022-08-14 13:23:53 +0300

    a

--- js/click.js
+++ js/click.js
@@ -1,27 +1,19 @@
-/* 
- * Love button for Design it & Code it
- * http://designitcodeit.com/i/9
- */
-$('.btn-counter').on('click', function(event, count) {
-  event.preventDefault();
-  
-  var $this = $(this),
-      count = $this.attr('data-count'),
-      active = $this.hasClass('active'),
-      multiple = $this.hasClass('multiple-count');
-  
-  // First method, allows to add custom function
-  // Use when you want to do an ajax request
-  /* if (multiple) {
-  $this.attr('data-count', ++count);
-  // Your code here
-  } else {
-  $this.attr('data-count', active ? --count : ++count).toggleClass('active');
-  // Your code here
-  } */
-  
-  // Second method, use when ... I dunno when but it looks cool and that's why it is here
-  $.fn.noop = $.noop;
-  $this.attr('data-count', ! active || multiple ? ++count : --count  )[multiple ? 'noop' : 'toggleClass']('active');
-  
-});
+      let likes = 0;
+      $(document).ready(function () {
+        // ajax to get current likes
+        // let likes from server are 10
+        // assign the current likes to variable
+        likes = 10;
+        setLikes(likes);
+      });
+      
+      $("body").on("click", ".likeBtn", function () {
+        // ajax to post a current likes
+        // in success add increment to likes
+        likes++;
+        setLikes(likes);
+      });
+
+      function setLikes(count) {
+        $(".totalLikes").text(count);
+      }
